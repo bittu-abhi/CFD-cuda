1933824064467fa941baa797773818a6a72f766d
Abhishek Sinha <abhisheksinha.iitkgp@gmail.com>
2017-07-22 17:07:01 +0530

    Changed the calculation of convective fluxes near cylinder wall.
    Also changed the way energy fluxes was being calculated

--- AUSM Plus/2D/Cylinder/Convflux.cu
+++ AUSM Plus/2D/Cylinder/Convflux.cu
@@ -1,25 +1,29 @@
 #include "ausmPlus.h"
 #include <math.h>
+#include <stdio.h>
 
 __global__ void convectiveflux(cell *domain, double *R, double *gammma)
 {
 	int y=threadIdx.x;
 	int x=blockIdx.x;
-	int faces=(int)domain[x].face[y][0];
-	int note;
-	if(domain[x].flag==0 || domain[x].flag==4)
+	int faces=(int)domain[x].face[y];
+	int ourFlag=(int)domain[x].flag;
+	int note=-1;
+	if(ourFlag==0 || ourFlag==4)
 	{
 		//Calculating the critical speed of sound for all the four sides/faces and the element itself
 		double a_s[2];
 
-		if(domain[x].face[y][0]<1 || domain[x].face[y][0]>26000)
+		if(domain[x].face[y]<1)
+		{
 			note=y;
+		}
 
 		//Element
 		a_s[0]=sqrt(2*(gammma[0]-1)/(gammma[0]+1)*(domain[x].stateVar[3]+(gammma[0]-1)*(domain[x].stateVar[3]\
 			-0.5*(pow(domain[x].stateVar[1],2)+pow(domain[x].stateVar[2],2))/domain[x].stateVar[0])));
 		//Side/face
-		if(domain[x].flag!=4)
+		if((int)ourFlag!=4 || ((int)ourFlag==4 && y!=note))
 			a_s[1]=sqrt(2*(gammma[0]-1)/(gammma[0]+1)*(domain[faces].stateVar[3]+(gammma[0]-1)*(domain[faces].stateVar[3]\
 			-0.5*(pow(domain[faces].stateVar[1],2)+pow(domain[faces].stateVar[2],2))/domain[faces].stateVar[0])));
 		else
@@ -29,69 +33,30 @@ __global__ void convectiveflux(cell *domain, double *R, double *gammma)
 
 		//speed for the boundary calculation
 		a_s[0]=pow(a_s[0],2)/max(a_s[0],abs(sqrt(pow(domain[x].stateVar[1]/domain[x].stateVar[0],2)+pow(domain[x].stateVar[2]/domain[x].stateVar[0],2))));
-		if(domain[x].flag!=4)
-			a_s[1]=pow(a_s[0],2)/max(a_s[0],abs(sqrt(pow(domain[faces].stateVar[1]/domain[faces].stateVar[0],2)\
+		if(ourFlag!=4 || (ourFlag==4 && y!=note))
+			a_s[1]=pow(a_s[1],2)/max(a_s[1],abs(sqrt(pow(domain[faces].stateVar[1]/domain[faces].stateVar[0],2)\
 			+pow(domain[faces].stateVar[2]/domain[faces].stateVar[0],2))));
 		else
 			a_s[1]=a_s[0];
-		
+
 		//Speed of sound at facial interface
 		double a_mid=min(a_s[0],a_s[1]);
 
-		//Pressure Fluxes
-		double pressplus=domain[x].stateVar[3]+(*gammma-1)*(domain[x].stateVar[3]-0.5*(pow(domain[x].stateVar[1],2)+pow(domain[x].stateVar[2],2))/domain[x].stateVar[0]);
-		double presminus;
-		if(domain[x].flag!=4)
-			presminus=(*gammma-1)*(domain[faces].stateVar[3]-0.5*(pow(domain[faces].stateVar[1],2)+pow(domain[faces].stateVar[2],2))/domain[faces].stateVar[0]);
-		else
-			presminus=(gammma[0]-1)*(domain[x].stateVar[3]-0.5*(pow(domain[x].stateVar[1],2)+pow(domain[x].stateVar[2],2))/domain[x].stateVar[0]);
+		//Pressure
+		double press=domain[x].stateVar[3]+(*gammma-1)*(domain[x].stateVar[3]-0.5*(pow(domain[x].stateVar[1],2)+pow(domain[x].stateVar[2],2))/domain[x].stateVar[0]);
 
-		int i1,i2;
-		if(domain[x].flag!=4)
-		{
-			for (int i = 0; i < 4; ++i)
-			{
-				for (int j = 0; j < 4; ++j)
-				{
-					if(domain[x].nodes[i][0]==domain[faces].nodes[j][0] && domain[x].nodes[i][1]==domain[faces].nodes[j][1])
-					{
-						if(domain[x].nodes[(i+1)%4][0]==domain[faces].nodes[(j+1)%4][0] && domain[x].nodes[(i+1)%4][1]==domain[faces].nodes[(j+1)%4][1])
-						{
-							i1=i;
-							i2=(i+1)%4;
-						}
-					}
-				}
-			}
-		}
-		else
-		{
-			for (int i = 0; i < 4; ++i)
-			{
-				if(domain[x].nodes[i][2]==domain[x].face[note][1] && domain[x].nodes[(i+1)%4][2]==domain[x].face[note][2])
-				{
-					i1=i;
-					i2=(i+1)%4;
-				}
-			}
-		}
-
-		//Machnumber of contravarient velocity
-		double machplus=(domain[x].stateVar[1]/domain[x].stateVar[0]*(domain[x].nodes[i1][0]-domain[x].nodes[i2][0])+\
-		domain[x].stateVar[2]/domain[x].stateVar[0]*(domain[x].nodes[i2][1]-domain[x].nodes[i1][1]))/a_mid/sqrt(pow(domain[x].nodes[i1][0]-domain[x].nodes[i2][0],2)\
-			+pow(domain[x].nodes[i2][1]-domain[x].nodes[i1][1],2));
+		//Machnumber of contravarient velocity(V=u*nx+v*ny)
+		double machplus=(domain[x].stateVar[1]/domain[x].stateVar[0]*domain[x].norms[y][0]+domain[x].stateVar[2]/domain[x].stateVar[0]*domain[x].norms[y][1])/a_mid;
 		double machminus;
-		if(domain[x].flag!=4)
+		if(ourFlag!=4 || (ourFlag==4 && y!=note))
 		{
-			machminus=(domain[faces].stateVar[1]/domain[faces].stateVar[0]*(domain[x].nodes[i1][0]-domain[x].nodes[i2][0])+\
-			domain[faces].stateVar[2]/domain[faces].stateVar[0]*(domain[x].nodes[i2][1]-domain[x].nodes[i1][1]))/a_mid/\
-			sqrt(pow(domain[x].nodes[i1][0]-domain[x].nodes[i2][0],2)+pow(domain[x].nodes[i2][1]-domain[x].nodes[i1][1],2));
+			machminus=(domain[faces].stateVar[1]/domain[faces].stateVar[0]*domain[x].norms[y][0]+domain[faces].stateVar[2]/domain[faces].stateVar[0]*domain[x].norms[y][1])/a_mid;
 		}
-			
 		else
 		{
-			machminus=machplus;
+			machminus=-machplus;
 		}
+
 		double split_mach_plus,split_mach_minus;
 
 		if(abs(machplus)>=1)
@@ -103,26 +68,31 @@ __global__ void convectiveflux(cell *domain, double *R, double *gammma)
 		else
 			split_mach_minus=-0.5*pow(machminus-1.0,2.0)-1/8*pow(pow(machminus,2.0)-1.0,2.0);
 
-		if(domain[x].flag!=4)
+		if(ourFlag!=4 || (ourFlag==4 && y!=note))
 		{
 			for (int i = 0; i < 4; ++i)
 			{
 				domain[x].convflux[y][i]=a_mid*(0.5*(split_mach_plus+abs(split_mach_plus))*domain[x].stateVar[i]+0.5*(split_mach_minus-abs(split_mach_minus))\
-					*domain[faces].stateVar[i]);
+					*domain[faces].stateVar[i])*sqrt(pow(domain[x].nodes[y][0]-domain[x].nodes[(y+1)%4][0],2)+pow(domain[x].nodes[y][1]-domain[x].nodes[(y+1)%4][1],2));
 			}
-			domain[x].convflux[y][3]+=a_mid*(0.5*(split_mach_plus+abs(split_mach_plus))*pressplus+0.5*(split_mach_minus-abs(split_mach_minus))*presminus);
+			domain[x].convflux[y][3]+=a_mid*(0.5*(split_mach_plus+abs(split_mach_plus))*press+0.5*(split_mach_minus-abs(split_mach_minus))*press)\
+			*sqrt(pow(domain[x].nodes[y][0]-domain[x].nodes[(y+1)%4][0],2)+pow(domain[x].nodes[y][1]-domain[x].nodes[(y+1)%4][1],2));
 		}
 		else
 		{
 			for (int i = 0; i < 4; ++i)
 			{
-				domain[x].convflux[y][i]=a_mid*(0.5*(split_mach_plus+abs(split_mach_plus))*domain[x].stateVar[i]+0.5*(split_mach_minus-abs(split_mach_minus))\
-					*domain[faces].stateVar[i]);
 				if(i==1 || i==2)
-					domain[x].convflux[y][i]=0;
-				
+					domain[x].convflux[y][i]=a_mid*(0.5*(split_mach_plus+abs(split_mach_plus))*domain[x].stateVar[i]-0.5*(split_mach_minus-abs(split_mach_minus))\
+					*domain[x].stateVar[i])*sqrt(pow(domain[x].nodes[y][0]-domain[x].nodes[(y+1)%4][0],2)+pow(domain[x].nodes[y][1]-domain[x].nodes[(y+1)%4][1],2));
+				else
+					domain[x].convflux[y][i]=a_mid*(0.5*(split_mach_plus+abs(split_mach_plus))*domain[x].stateVar[i]+0.5*(split_mach_minus-abs(split_mach_minus))\
+					*domain[x].stateVar[faces])*sqrt(pow(domain[x].nodes[y][0]-domain[x].nodes[(y+1)%4][0],2)+pow(domain[x].nodes[y][1]-domain[x].nodes[(y+1)%4][1],2));
 			}
-			domain[x].convflux[y][3]+=a_mid*(0.5*(split_mach_plus+abs(split_mach_plus))*pressplus+0.5*(split_mach_minus-abs(split_mach_minus))*presminus);
+			domain[x].convflux[y][3]+=a_mid*(0.5*(split_mach_plus+abs(split_mach_plus))*press+0.5*(split_mach_minus-abs(split_mach_minus))*press)\
+			*sqrt(pow(domain[x].nodes[y][0]-domain[x].nodes[(y+1)%4][0],2)+pow(domain[x].nodes[y][1]-domain[x].nodes[(y+1)%4][1],2));
 		}
+		//if(domain[x].flag==4)
+		//	printf("%lf %lf %d %d\n",domain[x].convflux[y][1],domain[x].convflux[y][2],x,y);
 	}
 }

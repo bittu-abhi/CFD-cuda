9eb378cabf590b2eb1337174d11556bd12ee42b9
Abhishek Sinha <abhisheksinha.iitkgp@gmail.com>
2017-07-22 17:05:10 +0530

    Added a mesh function to calculate face normals

--- AUSM Plus/2D/Cylinder/AUSM_2D.cu
+++ AUSM Plus/2D/Cylinder/AUSM_2D.cu
@@ -13,7 +13,7 @@ cell::cell(double *state)
 	omp_set_num_threads(4);
 	#pragma omp parallel for
 	for(int i=0;i<4;i++)
-	{
+	{	
 		stateVar[i]=state[i];
 	}
 }
@@ -24,16 +24,16 @@ __global__ void evaluate(cell *domain,double deltat)
 {
 	int x=blockIdx.x;
 	int y=threadIdx.x;
-	double vol=0.5*(domain[x].nodes[0][0]-domain[x].nodes[2][0])*(domain[x].nodes[1][1]-domain[x].nodes[3][1])+\
-	0.5*(domain[x].nodes[3][0]-domain[x].nodes[1][0])*(domain[x].nodes[0][1]-domain[x].nodes[2][1]);
+	double vol=abs(0.5*(domain[x].nodes[0][0]-domain[x].nodes[2][0])*(domain[x].nodes[1][1]-domain[x].nodes[3][1])+\
+	0.5*(domain[x].nodes[3][0]-domain[x].nodes[1][0])*(domain[x].nodes[0][1]-domain[x].nodes[2][1]));
 	if(domain[x].flag==0 || domain[x].flag==4)
 	{
-		domain[x].stateVar[y]=domain[x].stateVar[y]-(domain[x].convflux[0][y]+domain[x].convflux[1][y]+domain[x].convflux[2][y]+domain[x].convflux[3][y]\
+		domain[x].stateVar[y]-=(domain[x].convflux[0][y]+domain[x].convflux[1][y]+domain[x].convflux[2][y]+domain[x].convflux[3][y]\
 			-(domain[x].diffflux[0][y]+domain[x].diffflux[1][y]+domain[x].diffflux[2][y]+domain[x].diffflux[3][y]))/vol*deltat;
 		if(y==1)
-			domain[x].stateVar[y]+=(domain[x].presflux[0][0]+domain[x].presflux[1][0]+domain[x].presflux[2][0]+domain[x].presflux[3][0])/vol*deltat;
+			domain[x].stateVar[y]-=(domain[x].presflux[0][0]+domain[x].presflux[1][0]+domain[x].presflux[2][0]+domain[x].presflux[3][0])/vol*deltat;
 		if(y==2)
-			domain[x].stateVar[y]+=(domain[x].presflux[0][1]+domain[x].presflux[1][1]+domain[x].presflux[2][1]+domain[x].presflux[3][1])/vol*deltat;
+			domain[x].stateVar[y]-=(domain[x].presflux[0][1]+domain[x].presflux[1][1]+domain[x].presflux[2][1]+domain[x].presflux[3][1])/vol*deltat;
 	}
 }
 
@@ -51,7 +51,7 @@ __global__ void Boundary(cell *domain,double *initial)
 	{
 		if(threadIdx.x>0)
 		{
-			domain[x].stateVar[y]=2*domain[x].stateVar[y]-domain[(int)domain[x].face[3][0]].stateVar[y];
+			domain[x].stateVar[y]=2*domain[x].stateVar[y]-domain[(int)domain[x].face[3]].stateVar[y];
 		}
 		else
 			domain[x].stateVar[y]=initial[0];
@@ -65,10 +65,6 @@ __global__ void Boundary(cell *domain,double *initial)
 
 void ausmplus(double *initial,double timesteps, double deltat)
 {
-	//OpenMP flags
-	omp_set_nested(1);
-	omp_set_num_threads(2*omp_get_num_procs());
-
 	//GPU variables
 	cell *d_domain;
 	double *d_node,*d_boundary,*d_initial;
@@ -78,7 +74,7 @@ void ausmplus(double *initial,double timesteps, double deltat)
 	double *nodes=new double[25452*3];
 
 	//Values of boundary elements
-	double *boundary=new double[599*2];
+	double *boundary=new double[600*2];
 
 	//Alocate memory to domain
 	cell *domain=new cell[25000];
@@ -92,23 +88,45 @@ void ausmplus(double *initial,double timesteps, double deltat)
 	myfile2.open("Nodes.txt",ios::in);
 	myfile3.open("boundary.txt",ios::in);
 
+	double initial1[]={initial[0],initial[1],initial[2],initial[3]};
+
 	//Fill the array nodes for access in GPU
-	for(int i=0;i<25452*3;i+=3)
+	if(myfile2.is_open())
 	{
-		myfile2>>nodes[i]>>nodes[i+1]>>nodes[i+2];
+		for(int i=0;i<25452*3;i+=3)
+		{
+			myfile2>>nodes[i]>>nodes[i+1]>>nodes[i+2];
+		}
+	}
+	else
+	{
+		cout<<"Could not open Nodes.txt"<<endl;
 	}
 
 	//Fill the array boundary for access in GPU
-	for(int i=0;i<599*2;i+=2)
+	if(myfile3.is_open())
 	{
-		myfile3>>boundary[i]>>boundary[i+1];
+		for(int i=0;i<600*2;i+=2)
+		{
+			myfile3>>boundary[i]>>boundary[i+1];	
+		}
 	}
-
-	//Feed the file just once for access in GPU
-	for(int i=0;i<25000;i++)
+	else
 	{
-		domain[i]=cell(initial);
-		myfile1>>domain[i].nodes[0][2]>>domain[i].nodes[1][2]>>domain[i].nodes[2][2]>>domain[i].nodes[3][2];
+		cout<<"Could not open boundary.txt"<<endl;
+	}
+	//Feed the file just once for access in GPU
+	if(myfile1.is_open())
+	{
+		for(int i=0;i<25000;i++)
+		{	
+			domain[i]=cell(initial);
+			myfile1>>domain[i].nodes[0][2]>>domain[i].nodes[1][2]>>domain[i].nodes[2][2]>>domain[i].nodes[3][2];
+		}
+	}
+	else
+	{
+		cout<<"Could not open Elements.txt"<<endl;	
 	}
 	myfile1.close();
 	myfile2.close();
@@ -124,7 +142,7 @@ void ausmplus(double *initial,double timesteps, double deltat)
 	cudaStreamCreateWithFlags(&stream4,cudaStreamNonBlocking);
 	cudaMalloc((void **)&d_domain,25000*sizeof(cell));
 	cudaMalloc((void **)&d_node,25452*3*sizeof(double));
-	cudaMalloc((void **)&d_boundary,599*2*sizeof(double));
+	cudaMalloc((void **)&d_boundary,600*2*sizeof(double));
 	cudaMalloc((void **)&d_initial,4*sizeof(double));
 	cudaMalloc((void **)&d_R,sizeof(double));
 	cudaMalloc((void **)&d_k,sizeof(double));
@@ -136,8 +154,8 @@ void ausmplus(double *initial,double timesteps, double deltat)
 
 	cudaMemcpyAsync(d_domain,&domain[0],25000*sizeof(cell),cudaMemcpyHostToDevice,stream1);
 	cudaMemcpyAsync(d_node,&nodes[0],25452*3*sizeof(double),cudaMemcpyHostToDevice,stream2);
-	cudaMemcpyAsync(d_boundary,&boundary[0],599*2*sizeof(double),cudaMemcpyHostToDevice,stream3);
-	cudaMemcpyAsync(d_initial,&initial[0],4*sizeof(double),cudaMemcpyHostToDevice,stream3);
+	cudaMemcpyAsync(d_boundary,&boundary[0],600*2*sizeof(double),cudaMemcpyHostToDevice,stream3);
+	cudaMemcpyAsync(d_initial,&initial1[0],4*sizeof(double),cudaMemcpyHostToDevice,stream3);
 	cudaMemcpyAsync(d_R,&R,sizeof(double),cudaMemcpyHostToDevice,stream3);
 	cudaMemcpyAsync(d_k,&k,sizeof(double),cudaMemcpyHostToDevice,stream3);
 	cudaMemcpyAsync(d_gammma,&gammma,sizeof(double),cudaMemcpyHostToDevice,stream3);
@@ -148,7 +166,8 @@ void ausmplus(double *initial,double timesteps, double deltat)
 
 	set_nodes<<<25000,4>>>(d_node,d_domain,d_boundary);
 	set_neighbour<<<25000,4>>>(d_domain);
-	
+	calculate_norm<<<25000,4>>>(d_domain);
+
 	cudaDeviceSynchronize();
 
 	cout<<"Initialisation on the GPU : done"<<endl;
@@ -157,19 +176,22 @@ void ausmplus(double *initial,double timesteps, double deltat)
 	//Euler first order method
 	for (double t = 0; t < timesteps*deltat; t+=deltat)
 	{
-		pressureFlux<<<25000,4,0,stream1>>>(d_domain,d_R,d_gammma);
-		convectiveflux<<<25000,4,0,stream2>>>(d_domain,d_R,d_gammma);
-		diffusiveFlux<<<25000,4,0,stream3>>>(d_domain,d_R,d_gammma,d_mu,300,d_k);
+		pressureFlux<<<25000,4>>>(d_domain,d_R,d_gammma);
+		convectiveflux<<<25000,4>>>(d_domain,d_R,d_gammma);
+		diffusiveFlux<<<25000,4>>>(d_domain,d_R,d_gammma,d_mu,300,d_k);
 		cudaDeviceSynchronize();
 		evaluate<<<25000,4>>>(d_domain,deltat);
-		Boundary<<<25000,4,0,stream4>>>(d_domain,d_initial);
+		Boundary<<<25000,4>>>(d_domain,d_initial);
 		cudaDeviceSynchronize();
 		cout<<"time = "<<t<<endl;
+		cout<<endl;
 	}
 
 	cout<<endl;
 	cudaMemcpy(&domain[0],d_domain,25000*sizeof(cell),cudaMemcpyDeviceToHost);
-	cudaDeviceSynchronize();
+
+	//cudaDeviceSynchronize();
+
 	cout<<"Copying final values on the CPU from GPU : done"<<endl;
 	cout<<endl;
 
@@ -189,11 +211,11 @@ void ausmplus(double *initial,double timesteps, double deltat)
 	for (int i = 0; i < 25000; ++i)
 	{
 		//cout<<domain[i].diffflux[0][1]<<","<<domain[i].diffflux[1][1]<<","<<domain[i].diffflux[2][1]<<","<<domain[i].diffflux[3][1]<<endl;
-		cout<<"("<<domain[i].nodes[0][0]<<","<<domain[i].nodes[0][1]<<")"<<","<<"("<<domain[i].nodes[1][0]<<","<<domain[i].nodes[1][1]<<")";
-		cout<<"("<<domain[i].nodes[2][0]<<","<<domain[i].nodes[2][1]<<")"<<","<<"("<<domain[i].nodes[3][0]<<","<<domain[i].nodes[3][1]<<")"<<endl;
-		//cout<<domain[i].face[0][0]<<","<<domain[i].face[1][0]<<","<<domain[i].face[2][0]<<","<<domain[i].face[3][0]<<"		"<<domain[i].flag<<endl;
-		cout<<endl;
-	} 
+		cout<<"("<<domain[i].norms[0][0]<<","<<domain[i].norms[0][1]<<")"<<","<<"("<<domain[i].norms[1][0]<<","<<domain[i].norms[1][1]<<")";
+		cout<<"("<<domain[i].norms[2][0]<<","<<domain[i].norms[2][1]<<")"<<","<<"("<<domain[i].norms[3][0]<<","<<domain[i].norms[3][1]<<")"<<endl;
+		//cout<<domain[i].norms[0][1]<<","<<domain[i].norms[1][1]<<","<<domain[i].norms[2][1]<<","<<domain[i].norms[3][1]<<"		"<<domain[i].flag<<"	"<<i<<endl;
+		//cout<<endl;
+	}
 */
 	delete[] domain;
 }

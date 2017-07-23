507ce6254ab098ddc398fcd62e8eb80326f6bfa3
Abhishek Sinha <abhisheksinha.iitkgp@gmail.com>
2017-07-23 13:09:45 +0530

    Changed delta time

--- AUSM Plus/2D/Cylinder/Starter.cu
+++ AUSM Plus/2D/Cylinder/Starter.cu
@@ -21,14 +21,14 @@ int main()
 	//Rho
 	initial[0]=1.225;
 	//Rho*U
-	initial[1]=2;
+	initial[1]=0.1*initial[0];
 	//Rho *V
 	initial[2]=0;
 	//Rho*E, E is the internal energy including the kinetic energy(i.e. total intenal energy)
-	initial[3]=101325.;
+	initial[3]=101325/(gammma-1)+0.5*(pow(initial[1],2)+pow(initial[2],2))/initial[0];
 	//Time steps and delta_t
-	double timesteps=1;
-	double deltat=0.0001;
+	double timesteps=100;
+	double deltat=0.000001;
 	ausmplus(initial,timesteps,deltat);
 
 	return 0;

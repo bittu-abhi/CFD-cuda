2f0d4932e43abff45290c0d00a35181a487866f1
Abhishek Sinha <abhisheksinha.iitkgp@gmail.com>
2017-07-23 13:06:12 +0530

    Used gather state variables to p-flux

--- AUSM Plus/2D/Cylinder/Pressure.cu
+++ AUSM Plus/2D/Cylinder/Pressure.cu
@@ -1,68 +1,39 @@
 #include "ausmPlus.h"
 #include <math.h>
 #include <algorithm>
+#include <stdio.h>
 
 __global__ void pressureFlux(cell *domain, double *R, double *gammma)
 {
 	int y=threadIdx.x;
 	int x=blockIdx.x;
-	int faces=(int)domain[x].face[y];
-	int note=-1;
 	int ourFlag=(int)domain[x].flag;
 	if(ourFlag==0 || ourFlag==4)
 	{
 
 		//Calculating the critical speed of sound for all the four sides/faces and the element itself
 		double a_s[2];
-		if(domain[x].face[y]<1)
-		{
-			note=y;
-		}
-		
+
 		//Element
 		a_s[0]=sqrt(2*(gammma[0]-1)/(gammma[0]+1)*(domain[x].stateVar[3]+(gammma[0]-1)*(domain[x].stateVar[3]\
-			-0.5*(pow(domain[x].stateVar[1],2)+pow(domain[x].stateVar[2],2))/domain[x].stateVar[0])));
+			-0.5*(pow(domain[x].stateVar[1],2)+pow(domain[x].stateVar[2],2))/domain[x].stateVar[0]))/domain[x].stateVar[0]);
 		//Side/face
-		if(ourFlag!=4 || (ourFlag==4 && y!=note))
-			a_s[1]=sqrt(2*(gammma[0]-1)/(gammma[0]+1)*(domain[faces].stateVar[3]+(gammma[0]-1)*(domain[faces].stateVar[3]\
-			-0.5*(pow(domain[faces].stateVar[1],2)+pow(domain[faces].stateVar[2],2))/domain[faces].stateVar[0])));
-		else
-		{
-			a_s[1]=a_s[0];
-		}
+		a_s[1]=sqrt(2*(gammma[0]-1)/(gammma[0]+1)*(domain[x].temp_var[y][3]+(gammma[0]-1)*(domain[x].temp_var[y][3]\
+			-0.5*(pow(domain[x].temp_var[y][1],2)+pow(domain[x].temp_var[y][2],2))/domain[x].temp_var[y][0]))/domain[x].temp_var[y][0]);
 		
 		//speed for the boundary calculation
 		a_s[0]=pow(a_s[0],2)/max(a_s[0],abs(sqrt(pow(domain[x].stateVar[1]/domain[x].stateVar[0],2)+pow(domain[x].stateVar[2]/domain[x].stateVar[0],2))));
-		if(ourFlag!=4 || (ourFlag==4 && y!=note))
-			a_s[1]=pow(a_s[1],2)/max(a_s[1],abs(sqrt(pow(domain[faces].stateVar[1]/domain[faces].stateVar[0],2)+pow(domain[faces].stateVar[2]/domain[faces].stateVar[0],2))));
-		else
-		{
-			a_s[1]=a_s[0];
-		}
+		a_s[1]=pow(a_s[1],2)/max(a_s[1],abs(sqrt(pow(domain[x].temp_var[y][1]/domain[x].temp_var[y][0],2)+pow(domain[x].temp_var[y][2]/domain[x].temp_var[y][0],2))));
 
 		//Speed of sound at facial interface
 		double a_mid=min(a_s[0],a_s[1]);
 		//Mach number of incoming and outgoing waves
 		double machplus=(domain[x].stateVar[1]/domain[x].stateVar[0]*domain[x].norms[y][0]+domain[x].stateVar[2]/domain[x].stateVar[0]*domain[x].norms[y][1])/a_mid;
-		double machminus;
-		if(ourFlag!=4 || (ourFlag==4 && y!=note))
-		{
-			machminus=(domain[faces].stateVar[1]/domain[faces].stateVar[0]*domain[x].norms[y][0]+domain[faces].stateVar[2]/domain[faces].stateVar[0]*domain[x].norms[y][1])/a_mid;
-		}
-		else
-		{
-			machminus=-machplus;
-		}
+		double machminus=(domain[x].temp_var[y][1]/domain[x].temp_var[y][0]*domain[x].norms[y][0]+domain[x].temp_var[y][2]/domain[x].temp_var[y][0]*domain[x].norms[y][1])/a_mid;
 		
 		//Pressure Fluxes
 		double pressplus=(gammma[0]-1)*(domain[x].stateVar[3]-0.5*(pow(domain[x].stateVar[1],2)+pow(domain[x].stateVar[2],2))/domain[x].stateVar[0]);
-		double presminus;
-		if(ourFlag!=4 || (ourFlag==4 && y!=note))
-			presminus=(gammma[0]-1)*(domain[faces].stateVar[3]-0.5*(pow(domain[faces].stateVar[1],2)+pow(domain[faces].stateVar[2],2))/domain[faces].stateVar[0]);
-		else
-		{
-			presminus=(gammma[0]-1)*(domain[x].stateVar[3]-0.5*(pow(domain[x].stateVar[1],2)+pow(domain[x].stateVar[2],2))/domain[x].stateVar[0]);
-		}
+		double presminus=(gammma[0]-1)*(domain[x].temp_var[y][3]-0.5*(pow(domain[x].temp_var[y][1],2)+pow(domain[x].temp_var[y][2],2))/domain[x].temp_var[y][0]);
 
 		double plus,minus;
 		if(abs(machplus)>=1)
@@ -78,5 +49,8 @@ __global__ void pressureFlux(cell *domain, double *R, double *gammma)
 		+pow(domain[x].nodes[(y+1)%4][1]-domain[x].nodes[y][1],2));
 		domain[x].presflux[y][1]=(pressplus*plus+presminus*minus)*domain[x].norms[y][1]*sqrt(pow(domain[x].nodes[(y+1)%4][0]-domain[x].nodes[y][0],2)\
 		+pow(domain[x].nodes[(y+1)%4][1]-domain[x].nodes[y][1],2));
+
+		//if(domain[x].flag==4)
+		//	printf("%lf %lf %d %d\n",domain[x].presflux[y][0],domain[x].presflux[y][1],x,y );
 	}
 }

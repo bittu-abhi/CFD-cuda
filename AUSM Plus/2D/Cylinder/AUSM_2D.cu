09f323257226784d9a318d039a9d29af6d2c5c95
Abhishek Sinha <abhisheksinha.iitkgp@gmail.com>
2017-08-17 14:07:16 +0530

    Update AUSM_2D.cu

--- AUSM Plus/2D/Cylinder/AUSM_2D.cu
+++ AUSM Plus/2D/Cylinder/AUSM_2D.cu
@@ -7,15 +7,15 @@
 
 using namespace std;
 
-cell::cell(double *state)
+cell::cell(double *initial)
 {
-	omp_set_nested(1);
-	omp_set_num_threads(4);
-	#pragma omp parallel for
-	for(int i=0;i<4;i++)
-	{	
-		stateVar[i]=state[i];
-	}
+	//omp_set_nested(1);
+	//omp_set_num_threads(4);
+	//#pragma omp parallel for
+	stateVar[0]=initial[0];
+	stateVar[1]=0.0;
+	stateVar[2]=0.0;
+	stateVar[3]=(gammma-1)*(initial[3]-0.5*(pow(stateVar[1],2)+pow(stateVar[2],2))/stateVar[0]);
 }
 
 cell::cell(){}
@@ -24,16 +24,18 @@ __global__ void evaluate(cell *domain,double deltat)
 {
 	int x=blockIdx.x;
 	int y=threadIdx.x;
-	double vol=abs(0.5*(domain[x].nodes[0][0]-domain[x].nodes[2][0])*(domain[x].nodes[1][1]-domain[x].nodes[3][1])+\
-	0.5*(domain[x].nodes[3][0]-domain[x].nodes[1][0])*(domain[x].nodes[0][1]-domain[x].nodes[2][1]));
+	double vol=abs(0.500f*(domain[x].nodes[0][0]-domain[x].nodes[2][0])*(domain[x].nodes[1][1]-domain[x].nodes[3][1])+\
+	0.500f*(domain[x].nodes[3][0]-domain[x].nodes[1][0])*(domain[x].nodes[0][1]-domain[x].nodes[2][1]));
 	if(domain[x].flag==0 || domain[x].flag==4)
-	{
-		domain[x].stateVar[y]-=(domain[x].convflux[0][y]+domain[x].convflux[1][y]+domain[x].convflux[2][y]+domain[x].convflux[3][y])/vol*deltat;//\
-			-(domain[x].diffflux[0][y]+domain[x].diffflux[1][y]+domain[x].diffflux[2][y]+domain[x].diffflux[3][y]))/vol*deltat;
+	{	
+		
+		domain[x].stateVar[y]-=deltat*(domain[x].convflux[0][y]+domain[x].convflux[1][y]+domain[x].convflux[2][y]+domain[x].convflux[3][y]\
+		-(domain[x].diffflux[0][y]+domain[x].diffflux[1][y]+domain[x].diffflux[2][y]+domain[x].diffflux[3][y]))/vol;
+
 		if(y==1)
-			domain[x].stateVar[y]-=(domain[x].presflux[0][0]+domain[x].presflux[1][0]+domain[x].presflux[2][0]+domain[x].presflux[3][0])/vol*deltat;
+			domain[x].stateVar[y]-=deltat*(domain[x].presflux[0][0]+domain[x].presflux[1][0]+domain[x].presflux[2][0]+domain[x].presflux[3][0])/vol;
 		if(y==2)
-			domain[x].stateVar[y]-=(domain[x].presflux[0][1]+domain[x].presflux[1][1]+domain[x].presflux[2][1]+domain[x].presflux[3][1])/vol*deltat;
+		domain[x].stateVar[y]-=deltat*(domain[x].presflux[0][1]+domain[x].presflux[1][1]+domain[x].presflux[2][1]+domain[x].presflux[3][1])/vol;
 	}
 }
 
@@ -41,25 +43,42 @@ __global__ void Boundary(cell *domain,double *initial)
 {
 	int x=blockIdx.x;
 	int y=threadIdx.x;
-	//Inlet element evaluation
+	int note=-8;
+	int neigh1=-9,next=-8;
+
+	//Outlet element evaluation
 	if(domain[x].flag==1)
 	{
 		domain[x].stateVar[y]=initial[y];
 	}
-	//Outlet element evaluation
-	else if(domain[x].flag==2)
+	if(domain[x].flag==2)
 	{
-		if(threadIdx.x>0)
+		if(domain[x].face[y]<1 || domain[x].face[y]>25000)
 		{
-			domain[x].stateVar[y]=2*domain[x].stateVar[y]-domain[(int)domain[x].face[3]].stateVar[y];
+			note=y;
 		}
-		else
-			domain[x].stateVar[y]=initial[0];
-	}
-	//Farfield element evaluation
-	else if(domain[x].flag==3)
-	{
-		/* nothing to change :-) */
+			next=(int)domain[x].face[(y+2)%4]-1;
+		if(y==note)
+		{
+			for (int i = 0; i < 4; ++i)
+			{
+				if(domain[x].nodes[(y+2)%4][0]==domain[next].nodes[i][0] && domain[x].nodes[(y+2)%4][1]==domain[next].nodes[i][1] )
+				{
+					if(domain[x].nodes[(y+3)%4][0]==domain[next].nodes[(i+1)%4][0] && domain[x].nodes[(y+3)%4][1]==domain[next].nodes[(i+1)%4][1] )
+					{
+						neigh1=i;
+					}
+					else if(domain[x].nodes[(y+3)%4][0]==domain[next].nodes[(i+4-1)%4][0] && domain[x].nodes[(y+3)%4][1]==domain[next].nodes[(i+4-1)%4][1])
+					{
+						neigh1=(i-1+4)%4;
+					}
+				}
+			}
+			domain[x].stateVar[0]=initial[0];
+			domain[x].stateVar[1]=domain[next].stateVar[1];//-domain[next].temp_var[(neigh1+2)%4][1];
+			domain[x].stateVar[2]=domain[next].stateVar[2];//-domain[next].temp_var[(neigh1+2)%4][2];
+			domain[x].stateVar[3]=domain[next].stateVar[3];//-domain[next].temp_var[(neigh1+2)%4][3];
+		}
 	}
 }
 
@@ -74,7 +93,7 @@ void ausmplus(double *initial,double timesteps, double deltat)
 	double *nodes=new double[25452*3];
 
 	//Values of boundary elements
-	double *boundary=new double[600*2];
+	double *boundary=new double[599*2];
 
 	//Alocate memory to domain
 	cell *domain=new cell[25000];
@@ -87,8 +106,6 @@ void ausmplus(double *initial,double timesteps, double deltat)
 	myfile1.open("Elements.txt",ios::in);
 	myfile2.open("Nodes.txt",ios::in);
 	myfile3.open("boundary.txt",ios::in);
-
-	double initial1[]={initial[0],initial[1],initial[2],initial[3]};
 
 	//Fill the array nodes for access in GPU
 	if(myfile2.is_open())
@@ -106,7 +123,7 @@ void ausmplus(double *initial,double timesteps, double deltat)
 	//Fill the array boundary for access in GPU
 	if(myfile3.is_open())
 	{
-		for(int i=0;i<600*2;i+=2)
+		for(int i=0;i<599*2;i+=2)
 		{
 			myfile3>>boundary[i]>>boundary[i+1];	
 		}
@@ -142,7 +159,7 @@ void ausmplus(double *initial,double timesteps, double deltat)
 
 	cudaMalloc((void **)&d_domain,25000*sizeof(cell));
 	cudaMalloc((void **)&d_node,25452*3*sizeof(double));
-	cudaMalloc((void **)&d_boundary,600*2*sizeof(double));
+	cudaMalloc((void **)&d_boundary,599*2*sizeof(double));
 	cudaMalloc((void **)&d_initial,4*sizeof(double));
 	cudaMalloc((void **)&d_R,sizeof(double));
 	cudaMalloc((void **)&d_k,sizeof(double));
@@ -154,8 +171,8 @@ void ausmplus(double *initial,double timesteps, double deltat)
 
 	cudaMemcpyAsync(d_domain,&domain[0],25000*sizeof(cell),cudaMemcpyHostToDevice,stream1);
 	cudaMemcpyAsync(d_node,&nodes[0],25452*3*sizeof(double),cudaMemcpyHostToDevice,stream2);
-	cudaMemcpyAsync(d_boundary,&boundary[0],600*2*sizeof(double),cudaMemcpyHostToDevice,stream3);
-	cudaMemcpyAsync(d_initial,&initial1[0],4*sizeof(double),cudaMemcpyHostToDevice,stream3);
+	cudaMemcpyAsync(d_boundary,&boundary[0],599*2*sizeof(double),cudaMemcpyHostToDevice,stream3);
+	cudaMemcpyAsync(d_initial,&initial[0],4*sizeof(double),cudaMemcpyHostToDevice,stream3);
 	cudaMemcpyAsync(d_R,&R,sizeof(double),cudaMemcpyHostToDevice,stream3);
 	cudaMemcpyAsync(d_k,&k,sizeof(double),cudaMemcpyHostToDevice,stream3);
 	cudaMemcpyAsync(d_gammma,&gammma,sizeof(double),cudaMemcpyHostToDevice,stream3);
@@ -164,10 +181,10 @@ void ausmplus(double *initial,double timesteps, double deltat)
 	cout<<"Memory copy on the GPU : done"<<endl;
 	cout<<endl;
 
-	set_nodes<<<25000,4>>>(d_node,d_domain,d_boundary);
+	set_nodes<<<25000,4>>>(d_node,d_domain,d_boundary,d_initial);
 	set_neighbour<<<25000,4>>>(d_domain);
 	calculate_norm<<<25000,4>>>(d_domain);
-	read_values<<<25000,4,0,stream1>>>(d_domain);
+	read_values<<<25000,4>>>(d_domain);
 
 	cudaDeviceSynchronize();
 
@@ -188,6 +205,11 @@ void ausmplus(double *initial,double timesteps, double deltat)
 		cout<<endl;
 		read_values<<<25000,4,0,stream1>>>(d_domain);
 		cudaDeviceSynchronize();
+		if((int)(t/deltat)%50000==0)
+		{
+			cudaMemcpyAsync(&domain[0],d_domain,25000*sizeof(cell),cudaMemcpyDeviceToHost,stream3);
+			visual(domain,t);
+		}	
 	}
 
 	cout<<endl;
@@ -196,7 +218,7 @@ void ausmplus(double *initial,double timesteps, double deltat)
 	cout<<"Copying final values on the CPU from GPU : done"<<endl;
 	cout<<endl;
 
-	visual(domain);
+	visual(domain,deltat*timesteps);
 	cudaStreamDestroy(stream1);
 	cudaStreamDestroy(stream2);
 	cudaStreamDestroy(stream3);	
@@ -208,18 +230,5 @@ void ausmplus(double *initial,double timesteps, double deltat)
 
 	delete[] nodes;
 	delete[] boundary;
-	//delete[] initial1;
-
-/*
-	//For verification of the correct neighbours and nodes
-	for (int i = 0; i < 25000; ++i)
-	{
-		//cout<<domain[i].diffflux[0][1]<<","<<domain[i].diffflux[1][1]<<","<<domain[i].diffflux[2][1]<<","<<domain[i].diffflux[3][1]<<endl;
-		cout<<"("<<domain[i].norms[0][0]<<","<<domain[i].norms[0][1]<<")"<<","<<"("<<domain[i].norms[1][0]<<","<<domain[i].norms[1][1]<<")";
-		cout<<"("<<domain[i].norms[2][0]<<","<<domain[i].norms[2][1]<<")"<<","<<"("<<domain[i].norms[3][0]<<","<<domain[i].norms[3][1]<<")"<<endl;
-		//cout<<domain[i].norms[0][1]<<","<<domain[i].norms[1][1]<<","<<domain[i].norms[2][1]<<","<<domain[i].norms[3][1]<<"		"<<domain[i].flag<<"	"<<i<<endl;
-		//cout<<endl;
-	}
-*/
 	delete[] domain;
 }

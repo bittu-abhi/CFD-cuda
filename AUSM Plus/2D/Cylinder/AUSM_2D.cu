f765d7ee4bb501487a0b9fda5c06611334fa8054
Abhishek Sinha <abhisheksinha.iitkgp@gmail.com>
2017-08-28 18:04:59 +0530

    Updated to use unstructured

--- AUSM Plus/2D/Cylinder/AUSM_2D.cu
+++ AUSM Plus/2D/Cylinder/AUSM_2D.cu
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-cell::cell(double *initial)
+cell::cell(float *initial)
 {
 	//omp_set_nested(1);
 	//omp_set_num_threads(4);
@@ -15,88 +15,60 @@ cell::cell(double *initial)
 	stateVar[0]=initial[0];
 	stateVar[1]=0.0;
 	stateVar[2]=0.0;
-	stateVar[3]=(gammma-1)*(initial[3]-0.5*(pow(stateVar[1],2)+pow(stateVar[2],2))/stateVar[0]);
+	stateVar[3]=(gammma-1)*(initial[3]-0.5*(powf(stateVar[1],2)+powf(stateVar[2],2))/stateVar[0]);
 }
 
 cell::cell(){}
 
-__global__ void evaluate(cell *domain,double deltat)
+__global__ void evaluate(cell *domain,float deltat)
 {
 	int x=blockIdx.x;
 	int y=threadIdx.x;
-	double vol=abs(0.500f*(domain[x].nodes[0][0]-domain[x].nodes[2][0])*(domain[x].nodes[1][1]-domain[x].nodes[3][1])+\
-	0.500f*(domain[x].nodes[3][0]-domain[x].nodes[1][0])*(domain[x].nodes[0][1]-domain[x].nodes[2][1]));
-	if(domain[x].flag==0 || domain[x].flag==4)
+	float vol=0.5*abs((domain[x].nodes[0][0]-domain[x].nodes[1][0])*(domain[x].nodes[0][1]+domain[x].nodes[1][1])+\
+		(domain[x].nodes[1][0]-domain[x].nodes[2][0])*(domain[x].nodes[1][1]+domain[x].nodes[2][1])+\
+		(domain[x].nodes[2][0]-domain[x].nodes[0][0])*(domain[x].nodes[2][1]+domain[x].nodes[0][1]));
+	if(domain[x].flag==0 || domain[x].flag==4 || domain[x].flag==2)
 	{	
 		
-		domain[x].stateVar[y]-=deltat*(domain[x].convflux[0][y]+domain[x].convflux[1][y]+domain[x].convflux[2][y]+domain[x].convflux[3][y]\
-		-(domain[x].diffflux[0][y]+domain[x].diffflux[1][y]+domain[x].diffflux[2][y]+domain[x].diffflux[3][y]))/vol;
+		domain[x].stateVar[y]-=deltat*(domain[x].convflux[0][y]+domain[x].convflux[1][y]+domain[x].convflux[2][y]\
+		-(domain[x].diffflux[0][y]+domain[x].diffflux[1][y]+domain[x].diffflux[2][y]))/vol;
 
 		if(y==1)
-			domain[x].stateVar[y]-=deltat*(domain[x].presflux[0][0]+domain[x].presflux[1][0]+domain[x].presflux[2][0]+domain[x].presflux[3][0])/vol;
+			domain[x].stateVar[y]-=deltat*(domain[x].presflux[0][0]+domain[x].presflux[1][0]+domain[x].presflux[2][0])/vol;
 		if(y==2)
-		domain[x].stateVar[y]-=deltat*(domain[x].presflux[0][1]+domain[x].presflux[1][1]+domain[x].presflux[2][1]+domain[x].presflux[3][1])/vol;
+		domain[x].stateVar[y]-=deltat*(domain[x].presflux[0][1]+domain[x].presflux[1][1]+domain[x].presflux[2][1])/vol;
 	}
 }
 
-__global__ void Boundary(cell *domain,double *initial)
+__global__ void Boundary(cell *domain,float *initial)
 {
 	int x=blockIdx.x;
 	int y=threadIdx.x;
 	int note=-8;
-	int neigh1=-9,next=-8;
+	int next=-8;
 
 	//Outlet element evaluation
 	if(domain[x].flag==1)
 	{
 		domain[x].stateVar[y]=initial[y];
-	}
-	if(domain[x].flag==2)
-	{
-		if(domain[x].face[y]<1 || domain[x].face[y]>25000)
-		{
-			note=y;
-		}
-			next=(int)domain[x].face[(y+2)%4]-1;
-		if(y==note)
-		{
-			for (int i = 0; i < 4; ++i)
-			{
-				if(domain[x].nodes[(y+2)%4][0]==domain[next].nodes[i][0] && domain[x].nodes[(y+2)%4][1]==domain[next].nodes[i][1] )
-				{
-					if(domain[x].nodes[(y+3)%4][0]==domain[next].nodes[(i+1)%4][0] && domain[x].nodes[(y+3)%4][1]==domain[next].nodes[(i+1)%4][1] )
-					{
-						neigh1=i;
-					}
-					else if(domain[x].nodes[(y+3)%4][0]==domain[next].nodes[(i+4-1)%4][0] && domain[x].nodes[(y+3)%4][1]==domain[next].nodes[(i+4-1)%4][1])
-					{
-						neigh1=(i-1+4)%4;
-					}
-				}
-			}
-			domain[x].stateVar[0]=initial[0];
-			domain[x].stateVar[1]=domain[next].stateVar[1];//-domain[next].temp_var[(neigh1+2)%4][1];
-			domain[x].stateVar[2]=domain[next].stateVar[2];//-domain[next].temp_var[(neigh1+2)%4][2];
-			domain[x].stateVar[3]=domain[next].stateVar[3];//-domain[next].temp_var[(neigh1+2)%4][3];
-		}
-	}
+	}		
 }
 
-void ausmplus(double *initial,double timesteps, double deltat)
+void ausmplus(float *initial,float timesteps, float deltat)
 {
 	//GPU variables
 	cell *d_domain;
-	double *d_node,*d_boundary,*d_initial;
-	double *d_R,*d_k,*d_gammma,*d_mu;
+	float *d_node,*d_boundary,*d_initial;
+	float *d_R,*d_k,*d_gammma,*d_mu;
 
 	//Store the values of nodes for faster access
-	double *nodes=new double[25452*3];
+	float *nodes=new float[26233*3];
 
 	//Values of boundary elements
-	double *boundary=new double[599*2];
+	float *boundary=new float[2200*2];
 
 	//Alocate memory to domain
-	cell *domain=new cell[25000];
+	cell *domain=new cell[50266];
 
 	cout<<"Allocation on the host PC : done"<<endl;
 	cout<<endl;
@@ -110,7 +82,7 @@ void ausmplus(double *initial,double timesteps, double deltat)
 	//Fill the array nodes for access in GPU
 	if(myfile2.is_open())
 	{
-		for(int i=0;i<25452*3;i+=3)
+		for(int i=0;i<26233*3;i+=3)
 		{
 			myfile2>>nodes[i]>>nodes[i+1]>>nodes[i+2];
 		}
@@ -123,7 +95,7 @@ void ausmplus(double *initial,double timesteps, double deltat)
 	//Fill the array boundary for access in GPU
 	if(myfile3.is_open())
 	{
-		for(int i=0;i<599*2;i+=2)
+		for(int i=0;i<2200*2;i+=2)
 		{
 			myfile3>>boundary[i]>>boundary[i+1];	
 		}
@@ -135,10 +107,10 @@ void ausmplus(double *initial,double timesteps, double deltat)
 	//Feed the file just once for access in GPU
 	if(myfile1.is_open())
 	{
-		for(int i=0;i<25000;i++)
+		for(int i=0;i<50266;i++)
 		{	
 			domain[i]=cell(initial);
-			myfile1>>domain[i].nodes[0][2]>>domain[i].nodes[1][2]>>domain[i].nodes[2][2]>>domain[i].nodes[3][2];
+			myfile1>>domain[i].nodes[0][2]>>domain[i].nodes[1][2]>>domain[i].nodes[2][2];
 		}
 	}
 	else
@@ -157,34 +129,34 @@ void ausmplus(double *initial,double timesteps, double deltat)
 	cudaStreamCreate(&stream2);
 	cudaStreamCreate(&stream3);
 
-	cudaMalloc((void **)&d_domain,25000*sizeof(cell));
-	cudaMalloc((void **)&d_node,25452*3*sizeof(double));
-	cudaMalloc((void **)&d_boundary,599*2*sizeof(double));
-	cudaMalloc((void **)&d_initial,4*sizeof(double));
-	cudaMalloc((void **)&d_R,sizeof(double));
-	cudaMalloc((void **)&d_k,sizeof(double));
-	cudaMalloc((void **)&d_gammma,sizeof(double));
-	cudaMalloc((void **)&d_mu,sizeof(double));
+	cudaMalloc((void **)&d_domain,50266*sizeof(cell));
+	cudaMalloc((void **)&d_node,26233*3*sizeof(float));
+	cudaMalloc((void **)&d_boundary,2200*2*sizeof(float));
+	cudaMalloc((void **)&d_initial,4*sizeof(float));
+	cudaMalloc((void **)&d_R,sizeof(float));
+	cudaMalloc((void **)&d_k,sizeof(float));
+	cudaMalloc((void **)&d_gammma,sizeof(float));
+	cudaMalloc((void **)&d_mu,sizeof(float));
 
 	cout<<"Allocation on the GPU : done"<<endl;
 	cout<<endl;
 
-	cudaMemcpyAsync(d_domain,&domain[0],25000*sizeof(cell),cudaMemcpyHostToDevice,stream1);
-	cudaMemcpyAsync(d_node,&nodes[0],25452*3*sizeof(double),cudaMemcpyHostToDevice,stream2);
-	cudaMemcpyAsync(d_boundary,&boundary[0],599*2*sizeof(double),cudaMemcpyHostToDevice,stream3);
-	cudaMemcpyAsync(d_initial,&initial[0],4*sizeof(double),cudaMemcpyHostToDevice,stream3);
-	cudaMemcpyAsync(d_R,&R,sizeof(double),cudaMemcpyHostToDevice,stream3);
-	cudaMemcpyAsync(d_k,&k,sizeof(double),cudaMemcpyHostToDevice,stream3);
-	cudaMemcpyAsync(d_gammma,&gammma,sizeof(double),cudaMemcpyHostToDevice,stream3);
-	cudaMemcpyAsync(d_mu,&mu,sizeof(double),cudaMemcpyHostToDevice,stream3);
+	cudaMemcpyAsync(d_domain,&domain[0],50266*sizeof(cell),cudaMemcpyHostToDevice,stream1);
+	cudaMemcpyAsync(d_node,&nodes[0],26233*3*sizeof(float),cudaMemcpyHostToDevice,stream2);
+	cudaMemcpyAsync(d_boundary,&boundary[0],2200*2*sizeof(float),cudaMemcpyHostToDevice,stream3);
+	cudaMemcpyAsync(d_initial,&initial[0],4*sizeof(float),cudaMemcpyHostToDevice,stream3);
+	cudaMemcpyAsync(d_R,&R,sizeof(float),cudaMemcpyHostToDevice,stream3);
+	cudaMemcpyAsync(d_k,&k,sizeof(float),cudaMemcpyHostToDevice,stream3);
+	cudaMemcpyAsync(d_gammma,&gammma,sizeof(float),cudaMemcpyHostToDevice,stream3);
+	cudaMemcpyAsync(d_mu,&mu,sizeof(float),cudaMemcpyHostToDevice,stream3);
 
 	cout<<"Memory copy on the GPU : done"<<endl;
 	cout<<endl;
 
-	set_nodes<<<25000,4>>>(d_node,d_domain,d_boundary,d_initial);
-	set_neighbour<<<25000,4>>>(d_domain);
-	calculate_norm<<<25000,4>>>(d_domain);
-	read_values<<<25000,4>>>(d_domain);
+	set_nodes<<<50266,3>>>(d_node,d_domain,d_boundary,d_initial,d_gammma);
+	set_neighbour<<<50266,3>>>(d_domain);
+	calculate_norm<<<50266,3>>>(d_domain);
+	read_values<<<50266,3>>>(d_domain);
 
 	cudaDeviceSynchronize();
 
@@ -192,28 +164,29 @@ void ausmplus(double *initial,double timesteps, double deltat)
 	cout<<endl;
 
 	//Euler first order method
-	for (double t = 0; t < timesteps*deltat; t+=deltat)
+
+	for (float t = 0; t < timesteps*deltat; t+=deltat)
 	{
-		pressureFlux<<<25000,4,0,stream1>>>(d_domain,d_R,d_gammma);
-		convectiveflux<<<25000,4,0,stream2>>>(d_domain,d_R,d_gammma);
-		diffusiveFlux<<<25000,4,0,stream3>>>(d_domain,d_R,d_gammma,d_mu,300,d_k);
+		pressureFlux<<<50266,3,0,stream1>>>(d_domain,d_R,d_gammma);
+		convectiveflux<<<50266,3,0,stream2>>>(d_domain,d_R,d_gammma);
+		diffusiveFlux<<<50266,3,0,stream3>>>(d_domain,d_R,d_gammma,d_mu,300,d_k);
 		cudaDeviceSynchronize();
-		evaluate<<<25000,4>>>(d_domain,deltat);
-		Boundary<<<25000,4,0,stream1>>>(d_domain,d_initial);
+		if((int)(t/deltat)%1000000==0)
+		{
+			cudaMemcpyAsync(&domain[0],d_domain,50266*sizeof(cell),cudaMemcpyDeviceToHost,stream3);
+			visual(domain,t);
+		}
+		evaluate<<<50266,4>>>(d_domain,deltat);
+		//Boundary<<<50266,3,0,stream1>>>(d_domain,d_initial);
 		cudaDeviceSynchronize();
 		cout<<"time = "<<t<<endl;
 		cout<<endl;
-		read_values<<<25000,4,0,stream1>>>(d_domain);
+		read_values<<<50266,3,0,stream1>>>(d_domain);
 		cudaDeviceSynchronize();
-		if((int)(t/deltat)%50000==0)
-		{
-			cudaMemcpyAsync(&domain[0],d_domain,25000*sizeof(cell),cudaMemcpyDeviceToHost,stream3);
-			visual(domain,t);
-		}	
 	}
 
 	cout<<endl;
-	cudaMemcpy(&domain[0],d_domain,25000*sizeof(cell),cudaMemcpyDeviceToHost);
+	cudaMemcpy(&domain[0],d_domain,50266*sizeof(cell),cudaMemcpyDeviceToHost);
 
 	cout<<"Copying final values on the CPU from GPU : done"<<endl;
 	cout<<endl;

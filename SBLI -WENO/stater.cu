6bca90fd7c0416375dc358423d35e214ec9c97c3
Abhishek Sinha <abhisheksinha.iitkgp@gmail.com>
2017-10-25 02:25:48 +0530

    Starting parameters
    Parameters like delta t, delta x, delta y timesteps etc are provided here.

---
+++ SBLI -WENO/stater.cu
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "sbli.h"
+
+using namespace std;
+
+int main()
+{
+	double u = 1000;
+	double rho = ;
+	double v = ;
+	double e = ;
+	double reynolds = ;
+	double prandtl = ;
+
+	double deltat = ;
+	double deltax = ;
+	double deltay = ;
+	double timesteps = ;
+
+	sbli();
+
+	return 0;
+}
